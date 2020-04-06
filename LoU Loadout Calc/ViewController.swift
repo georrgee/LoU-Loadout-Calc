4d2ee9ad7beeee00cab85092bbff9615374d69f6
georrgee <garcia.george.b@gmail.com>
2020-04-06 14:46:00 -0700

    Initial Commit

---
+++ LoU Loadout Calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoU Loadout Calc
+//
+//  Created by George Garcia on 4/6/20.
+//  Copyright © 2020 GeeTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
