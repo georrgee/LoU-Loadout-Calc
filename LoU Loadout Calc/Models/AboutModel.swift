e2f392d387836dfd680489757da85dbf83676b41
Billy Pak <pak.billy@gmail.com>
2020-11-10 23:12:14 -0800

    added app version onto the app

--- LoU Loadout Calc/Models/AboutModel.swift
+++ LoU Loadout Calc/Models/AboutModel.swift
@@ -10,8 +10,16 @@ struct AboutModel {
     let iconImage: UIImage
     let title:     String
     
+    
     static let list: [AboutModel] = [
         AboutModel(iconImage: LastOfUsImages.AboutIcons.settingsIcon!, title: "SETTINGS"),
-        AboutModel(iconImage: LastOfUsImages.AboutIcons.developerIcon!, title: "DEVELOPED BY PERUCAN")
+        AboutModel(iconImage: LastOfUsImages.AboutIcons.developerIcon!, title: "DEVELOPED BY PERUCAN"),
+        AboutModel(iconImage: LastOfUsImages.AboutIcons.appVersionIcon!, title: "APP VERSION:  \(appVersion())")
     ]
+}
+
+func appVersion() -> String {
+    guard let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else { return "something went wrong" }
+
+    return appVersion
 }
